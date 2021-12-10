fcb02e4bc51a0211c6a7d366a10efcb18f408d77
ArtMoc <tem.mochalov@yandex.ru>
2021-12-10 12:09:24 +0300

    Clear ForwardList in progress

--- ForwardList/main.cpp
+++ ForwardList/main.cpp
@@ -8,17 +8,22 @@ class Element
 {
 	int Data; //значение элемента
 	Element* pNext; //адрес следующего элемента
+	static int count; // количество элементов
 public:
 	Element(int Data, Element* pNext = nullptr) :Data(Data), pNext(pNext)
 	{
+		count++;
 		cout << "E_Constructor:\t" << this << endl;
 	}
 	~Element()
 	{
+		count--;
 		cout << "E_Destructor:\t" << this << endl;
 	}
 	friend class ForwardList;
 };
+int Element::count = 0; //статические переменные могут быть проинициализированы только
+                        //за классом
 
 class ForwardList
 {
@@ -31,9 +36,16 @@ public:
 	}
 	~ForwardList()
 	{
-		cout << "LD_Destructor:\t" << this << endl;
+		//for ()
+		{
+			Element* Temp = Head;
+			Head = Head->pNext;
+			delete Temp;
+		}
+		
+		cout << "FL_Destructor:\t" << this << endl;
 	}
-	
+
 	//                        ADDING ELEMENTS:
 	void push_front(int Data)
 	{
@@ -44,16 +56,65 @@ public:
 		//3) Переносим голову на новый элемент:
 		Head = New;
 	}
-	void pop_front()
-	{
-		Element* Temp = Head->pNext;
-		delete Head;
-		Head = Temp;
-	} 
 	void push_back(int Data)
 	{
-		Element* New = new Element(Data);
+		Element* Temp = Head;
+		while (Temp->pNext)Temp = Temp->pNext;
+		Temp->pNext = new Element(Data);
+	}
 
+	//                     Erasing elements:
+	void pop_front()
+	{
+		// 1) запоминаем адрес удаляемого элемента
+		Element* Erased = Head;
+		// 2) исключаем элемент из списка
+		Head = Head->pNext;
+		// 3) удаляем элемент из списка
+		delete Erased;
+	}
+	void pop_back()
+	{
+		// 1) доходим до предпоследнего элемента
+		Element* Temp = Head;
+		while (Temp->pNext->pNext)Temp = Temp->pNext;//оказались в предпоследнем элементе
+		// 2) удаляем последний элемент из памяти
+		delete Temp->pNext;
+		// 3) "забываем" об удаленном элементе,то есть затираем его адрес нулем
+		Temp->pNext = nullptr;
+		//Теперь Temp является последним элементом списка
+	}
+	void insert(int Data, int Index)
+	{
+		if (Index > Head->count)
+		{
+			cout << "Error: Выход за пределы списка." << endl;
+			return;
+		}
+		if (Index == 0 || Head == nullptr)return push_front(Data);
+		// 0) создаем новый элемент
+		Element* New = new Element(Data);
+		// 1) доходим до нужного элемента
+		Element* Temp = Head;
+		for (int i = 0; i < Index - 1; i++)Temp = Temp->pNext;
+		// осуществялем вставку нового элемента
+		// 2) привязываем новый элемень к списку 
+		New->pNext = Temp->pNext;
+		// 3) включаем элемент в список
+		Temp->pNext = New;
+	}
+	void erase(int Index)
+	{
+		if (Index > Head->count)
+		{
+			cout << "Error: Выход за пределы списка." << endl;
+			return;
+		}
+		Element* Temp = Head; //доходим до нужного элемента
+		for (int i = 0; i < Index - 1; i++)Temp = Temp->pNext;
+		Element* New = Temp->pNext;
+		Temp->pNext = Temp->pNext->pNext;
+		delete New;
 	}
 
 
@@ -61,12 +122,13 @@ public:
 	void print()const
 	{
 		Element* Temp = Head; //Temp - ИТЕРАТОР (УКАЗАТЕЛЬ, ПРИ ПОМОЩИ КОТОРОГО
-		                      //МОЖНО ПОЛУЧИТЬ ДОСТУП К ЭЛЕМЕНТАМ СТРУКТУРЫ ДАННЫХ
-		while (Temp!=nullptr)
+							  //МОЖНО ПОЛУЧИТЬ ДОСТУП К ЭЛЕМЕНТАМ СТРУКТУРЫ ДАННЫХ
+		while (Temp != nullptr)
 		{
 			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
 			Temp = Temp->pNext; //Переход на следующий элемент
 		}
+		cout << "Количество элементов списка: " << Head->count << endl;
 	}
 };
 
@@ -82,6 +144,23 @@ void main()
 	}
 	list.print();
 	cout << delimiter << endl;
+	list.push_back(123);
+	list.print();
+	cout << delimiter << endl;
 	list.pop_front();
+	list.print();
+	cout << delimiter << endl;
+	list.pop_back();
+	list.print();
+	cout << delimiter << endl;
+	int index;
+	int value;
+	cout << "Введите индекс добавляемого элемента: ";cin >> index;
+	cout << "Введите значение добавляемого элемента: ";cin >> value;
+	list.insert(value, index);
+	list.print();
+	cout << delimiter << endl;
+	cout << "Введите индекс удаляемого элемента: ";cin >> index;
+	list.erase(index);
 	list.print();
 }
